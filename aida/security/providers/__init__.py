88b26e9422ed299101145d9ad27e75dbaa06ada9
Whiplash <145601386+Whiplash861@users.noreply.github.com>
2026-07-17 12:00:10 -0400

    Export Microsoft Defender provider

--- aida/security/providers/__init__.py
+++ aida/security/providers/__init__.py
@@ -1,5 +1,17 @@
 """Antivirus provider interfaces and adapters."""
 
-from aida.security.providers.base import AntivirusProvider, UnsupportedAntivirusProvider
+from aida.security.providers.base import (
+    AntivirusProvider,
+    UnsupportedAntivirusProvider,
+)
+from aida.security.providers.defender import (
+    MicrosoftDefenderError,
+    MicrosoftDefenderProvider,
+)
 
-__all__ = ["AntivirusProvider", "UnsupportedAntivirusProvider"]
+__all__ = [
+    "AntivirusProvider",
+    "MicrosoftDefenderError",
+    "MicrosoftDefenderProvider",
+    "UnsupportedAntivirusProvider",
+]
