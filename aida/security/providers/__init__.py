792b28b5e4fe7a5184e26f3bcdb30ed01ab6ad39
Whiplash <145601386+Whiplash861@users.noreply.github.com>
2026-07-17 11:23:09 -0400

    Expose antivirus provider API

---
+++ aida/security/providers/__init__.py
@@ -0,0 +1,5 @@
+"""Antivirus provider interfaces and adapters."""
+
+from aida.security.providers.base import AntivirusProvider, UnsupportedAntivirusProvider
+
+__all__ = ["AntivirusProvider", "UnsupportedAntivirusProvider"]
