a6c4c45a4ef8ef35ea90d13e8cdc1e7035f569cd
Whiplash <145601386+Whiplash861@users.noreply.github.com>
2026-09-04 12:48:55 -0400

    Add Sentry package foundation

---
+++ aida/aegis/sentry/__init__.py
@@ -0,0 +1,17 @@
+from aida.aegis.sentry.models import (
+    SentryAttackPlan,
+    SentryAttackResult,
+    SentryAttackState,
+    SentryProcessTarget,
+    SentrySessionTarget,
+)
+from aida.aegis.sentry.protocol import SentryAttackService
+
+__all__ = [
+    "SentryAttackPlan",
+    "SentryAttackResult",
+    "SentryAttackService",
+    "SentryAttackState",
+    "SentryProcessTarget",
+    "SentrySessionTarget",
+]
