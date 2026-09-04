a3487ac930e32da76fc2f232dcb3f8f47c47169d
Whiplash <145601386+Whiplash861@users.noreply.github.com>
2026-09-04 13:01:46 -0400

    Expose remote intrusion and guarded containment patterns to Artificer

--- aida/aegis/engineering_manifest.py
+++ aida/aegis/engineering_manifest.py
@@ -5,7 +5,7 @@ from typing import Any
 from aida.aegis.learning.service import AegisLearningService
 
 
-MANIFEST_VERSION = "1.0"
+MANIFEST_VERSION = "1.1"
 
 
 def build_engineering_manifest(
@@ -33,6 +33,11 @@ def build_engineering_manifest(
             "poisoning_resistant_training_gate",
             "versioned_feature_schema",
             "versioned_model_lifecycle",
+            "remote_intrusion_correlation",
+            "authorized_support_disambiguation",
+            "exact_identity_active_containment",
+            "fresh_scope_bound_confirmation",
+            "post_containment_verification",
             "shadow_model_ready_architecture",
             "rollback_ready_model_contract",
         ),
@@ -53,6 +58,7 @@ def build_engineering_manifest(
             "hashes": False,
             "network_endpoints": False,
             "command_lines": False,
+            "support_vendor_labels": False,
             "security_case_contents": False,
             "user_conversation": False,
         },
@@ -60,6 +66,11 @@ def build_engineering_manifest(
             "learning_grants_execution_authority": False,
             "learned_inference_overrides_provider_evidence": False,
             "learned_inference_overrides_policy": False,
+            "remote_support_authorization_is_security_whitelist": False,
+            "sentry_requires_local_user_attacker_confirmation": True,
+            "sentry_requires_fresh_exact_confirmation": True,
+            "sentry_network_isolation_enabled": False,
+            "sentry_file_deletion_enabled": False,
         },
     }
 
